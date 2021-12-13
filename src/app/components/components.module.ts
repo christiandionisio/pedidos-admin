5ae8b5219067c5ce49791720bbac716505b75926
Christian Dionisio <christiandionisio9432@gmail.com>
2021-12-12 22:36:33 -0500

    Diseno producto page y diseno modal

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { ModalProductoComponent } from './modal-producto/modal-producto.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    ModalProductoComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    ModalProductoComponent
   ]
 })
 export class ComponentsModule { }
