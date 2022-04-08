3305415960c19d44e0c2626b77ec0ba3e1add692
Christian Dionisio <christiandionisio9432@gmail.com>
2022-04-07 19:20:16 -0500

    Fix navbar toggle

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
 
@@ -13,7 +13,8 @@ export class NavbarComponent implements OnInit, OnDestroy {
   private ngUnsubscribe = new Subject();
   public username = localStorage.getItem('user');
 
-  constructor(private router: Router) { 
+  constructor(private router: Router,
+      private renderer: Renderer2) { 
     this.router.events.subscribe(event => this.setUrlToBreadcrumb());
   }
   
@@ -28,6 +29,14 @@ export class NavbarComponent implements OnInit, OnDestroy {
   setUrlToBreadcrumb() {
     this.arrayPath = this.router.url.split('/');
     // console.log(this.arrayPath);
+  }
+
+  handleToggle() {
+    if (document.body.classList.contains('g-sidenav-pinned')) {
+      this.renderer.removeClass(document.body, 'g-sidenav-pinned');
+    } else {
+      this.renderer.addClass(document.body, 'g-sidenav-pinned');
+    }
   }
 
 
